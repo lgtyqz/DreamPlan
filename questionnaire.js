ade763175ccb486a05045128f59006b72f0e733b
lgtyqz <kevinlyang01@gmail.com>
2019-10-12 20:57:25 -0700

    Added questionnaire form

--- questionnaire.js
+++ questionnaire.js
@@ -1,8 +1,11 @@
 var subject = {
+    collectionMethod: "paths",
+    query: "",
     logos: 0, //The research-based path
     ethos: 0, //The service-based path
     pathos: 0 //The collaboration-based/social path
 };
+var paths = ["logos", "ethos", "pathos"];
 var questionIndex = 0;
 var questions = [
     new Question("Arts, sciences, or both?",
@@ -40,10 +43,10 @@ var questions = [
     "My academic writing skills",
     "My verbal communication skills"]
     ),
-    new Question("What do you want to improve on as a student?",
-    ["My researching skills",
-    "My academic writing skills",
-    "My verbal communication skills"]
+    new Question("What kind of people and subjects do you follow on social media?",
+    ["Artistic accounts",
+    "Activism/Social Change accounts",
+    "Entertainment accounts"]
     ),
 ];
 function Question(q, a){
@@ -53,8 +56,45 @@ function Question(q, a){
     this.answers = a;
 }
 function displayQuestion(){
-
+    document.getElementById("feedback").innerHTML = "";
+    document.getElementById("question").innerHTML = questions[questionIndex].question;
+    for(var i = 1; i <= 3; i++){
+        document.getElementById("a" + i.toString()).innerHTML = questions[questionIndex].answers[i - 1];
+    }
 }
-function processQuestion(){
-
+function processAnswer(){
+    //First check whether stuff is answered.
+    var answered = false;
+    var selected = null;
+    for(var i = 1; i <= 3; i++){
+        var selector = document.getElementById("r" + i.toString());
+        if(selector.checked){
+            answered = true;
+            selected = i - 1;
+        }
+        selector.checked = false;
+    }
+    if(!answered){
+        document.getElementById("feedback").innerHTML =
+            "You must select an answer to move on!";
+    }else{
+        if(subject.collectionMethod == "paths"){
+            subject[paths[selected]]++;
+        }else if(subject.collectionMethod == "query"){
+            //TODOOOOOO
+        }
+        // Process and display the next question, or go to the next page if the
+        // questionnaire is finished.
+        questionIndex++;
+        if(questionIndex == questions.length - 1){
+            //Change button text from "Continue" to "Finish!"
+        }else if(questionIndex >= questions.length){
+            //Redirect to next page. Add URL parameters.
+        }else{
+            displayQuestion();
+        }
+    }
+}
+window.onload = function(){
+    displayQuestion(questionIndex);
 }
