0cb4a5c2b264ed8ad277e22c87d5b53d226caf1e
lgtyqz <kevinlyang01@gmail.com>
2019-10-13 00:09:05 -0700

    Beginnings of result-loading

--- questionnaire.js
+++ questionnaire.js
@@ -1,5 +1,5 @@
 var subject = {
-    collectionMethod: "paths",
+    collectionMethod: "query",
     query: "",
     logos: 0, //The research-based path
     ethos: 0, //The service-based path
@@ -11,49 +11,76 @@ var questions = [
     new Question("Arts, sciences, or both?",
     ["Arts",
     "Sciences",
-    "Both"]
+    "Both"],
+    ["arts, humanities",
+    "science, math",
+    "interdisciplinary arts science"]
     ),
     new Question("In your opinion, what subject isn't discussed enough in school?",
     ["The importance of knowledge retention vs. memorization",
     "The importance of delving into what you value as a person",
-    "The importance of building solid communication skills"]
+    "The importance of building solid communication skills"],
+    ["study skills",
+    "ethics",
+    "communications"]
     ),
     new Question("Which of the three activities would you most want on your bucket list?",
     ["Contributing a groundbreaking scientific discovery",
     "Giving a TED Talk on a pressing social issue",
-    "Writing and publishing an autobiography"]
+    "Writing and publishing an autobiography"],
+    ["science research",
+    "sociology",
+    "writing"]
     ),
     new Question("Which do you consider most important in conflict resolution?",
     ["All of the facts being acknowledged by both sides",
     "Articulating the individual impact both sides have had in the conflict",
-    "Making sure both sides empathize with each other to reach mutual understanding"]
+    "Making sure both sides empathize with each other to reach mutual understanding"],
+    ["research",
+    "history",
+    "sociology diplomacy"]
     ),
     new Question("Which of the three things would you want to change in the world first?",
     ["Eliminating disease",
     "Homelessness and hunger",
-    "Working towards world peace"]
+    "Working towards world peace"],
+    ["biology",
+    "urban studies",
+    "global studies, international studies"]
     ),
     new Question("What do you read the most in your free time?",
     ["educational books and articles",
     "the news",
-    "others' personal stories"]
+    "others' personal stories"],
+    ["research",
+    "current events",
+    "literature"]
     ),
     new Question("What do you want to improve on as a student?",
     ["My researching skills",
     "My academic writing skills",
-    "My verbal communication skills"]
+    "My verbal communication skills"],
+    ["research",
+    "writing",
+    "debate"]
     ),
     new Question("What kind of people and subjects do you follow on social media?",
     ["Artistic accounts",
     "Activism/Social Change accounts",
-    "Entertainment accounts"]
+    "Entertainment accounts"],
+    ["art",
+    "activism",
+    "media"]
     ),
 ];
-function Question(q, a){
+function Question(q, a, keywords){
     //q is the question (a String)
     //a are the answers (an array of Strings)
+    //keywords is a list of the keywords associated with the corresponding index
+    //in a
     this.question = q;
     this.answers = a;
+    this.keywords = keywords;
 }
 function displayQuestion(){
     document.getElementById("feedback").innerHTML = "";
@@ -81,7 +108,8 @@ function processAnswer(){
         if(subject.collectionMethod == "paths"){
             subject[paths[selected]]++;
         }else if(subject.collectionMethod == "query"){
-            //TODOOOOOO
+            subject.query += questions[questionIndex].keywords[selected];
+            subject.query += ", ";
         }
         // Process and display the next question, or go to the next page if the
         // questionnaire is finished.
--- results.js
+++ results.js
@@ -1,8 +1,27 @@
 // Read current URL parameters and adjust the UI accordingly.
 // Basically a placeholder for any future server-based stuff.
 var url = new URL(window.location);
+var objectResponse = null;
 var keywords = url.searchParams.get("keywords");
-window.onload = function(){
-    keywords = this.decodeURIComponent(keywords);
+keywords = this.decodeURIComponent(keywords);
+
+var baseURL = "https://dreamplan.search.windows.net/indexes/catalog-json-index/docs?api-version=2019-05-06&search=" + keywords;
+var apiKey = new Headers();
+apiKey.append('api-key', "63686663099E6433AFC3694E2F0D22E0");
+apiKey.append('content-type', 'application/json');
+var init = {
+    method: 'GET',
+    headers: apiKey,
+    mode: 'no-cors'
+};
+var x = fetch(baseURL, init);
+x.then(
+    function(response){
+        document.getElementById("form").innerHTML = response;
+        objectResponse = response;
+        //TODO: Format data
+    }
+);
+/*window.onload = function(){
     document.getElementById("form").innerHTML = keywords;
-}
+}*/
